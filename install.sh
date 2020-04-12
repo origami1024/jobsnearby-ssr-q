86186aa6bd74c76f42f7661723be3f47e787a13b
origami <origami1024@gmail.com>
2020-04-12 22:36:44 +0300

    deploy x1

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo  ' - Alright, here we go. trigerring auto deploy .. ..'  &&  echo
+
+installDependencies() {
+  echo  " Installing global dependencies.."  &&       echo
+  npm install
+}
+
+
+
+buildSSR() {
+  echo  " Installing server dependencies .. .."
+  quasar build -m ssr
+}
+
+
+
+setupClient() {
+  echo  " Installing application dependencies .. .."  &&  echo
+  echo dir .
+  echo " -------- "
+  cd ./dist/ssr
+  npm install
+}
+
+installDependencies && buildSSR && setupClient && copy
